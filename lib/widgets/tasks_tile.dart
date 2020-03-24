2782b4ea35b0604f15672b053147f34f51aef2fc
Marco Leonardini <marco_leonardini@hotmail.com>
2020-03-24 18:13:15 -0400

    Added Local State

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  final bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('Todo 1'),
-      trailing: Checkbox(onChanged: (value) {}, value: false),
+      trailing: TaskCheckBox(),
+    );
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  @override
+  _TaskCheckBoxState createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue;
+        });
+      },
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
     );
   }
 }
